c0b4dabb55775548c73aa1ecce5d23b9b48c250a
Hao Lian <me@haolian.org>
2014-09-28 23:44:33 -0400

    SongView refactored out of SongViewController

--- Syncopate/HomeViewController.swift
+++ Syncopate/HomeViewController.swift
@@ -29,12 +29,15 @@ class HomeViewController: UIViewController {
     }
 
     override func viewWillAppear(animated: Bool) {
-        self.addFaun(SongViewController(song: Song(url: NSURL(string: "https://a.tumblr.com/tumblr_mndqjdrkkq1s1b8mno1_r1.mp3"))))
-        self.addFaun(SongViewController(song: Song(url: NSURL(string: "https://a.tumblr.com/tumblr_naqik7VOSl1te74f8o1.mp3"))))
+        self.addFaun(SongViewController(
+            frame: CGRectMake(10, CGFloat(10), self.view.bounds.size.width - 20, CGFloat(self.songHeight)),
+            song: Song(url: NSURL(string: "https://a.tumblr.com/tumblr_mndqjdrkkq1s1b8mno1_r1.mp3"))))
+        self.addFaun(SongViewController(
+            frame: CGRectMake(10, CGFloat(self.songHeight + 20), self.view.bounds.size.width - 20, CGFloat(self.songHeight)),
+            song: Song(url: NSURL(string: "https://a.tumblr.com/tumblr_naqik7VOSl1te74f8o1.mp3"))))
 
         for i in 0...(self.fauns.count - 1) {
             let songFaun = fauns[i] as SongViewController
-            songFaun.view.frame = CGRectMake(10, CGFloat(self.songHeight * i + 10 * (i + 1)), self.view.bounds.size.width - 20, CGFloat(self.songHeight))
             self.view.addSubview(songFaun.view)
             songFaun.didMoveToParentViewController(self)
         }
--- Syncopate/SongViewController.swift
+++ Syncopate/SongViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
+class SongView : UIView {
+    let song : Song
+
+    required init(coder: NSCoder) {
+        assertionFailure("no")
+    }
+
+    required init(frame: CGRect, song: Song, playingSignal: RACSignal) {
+        self.song = song
+
+        super.init(frame: frame)
+
+        playingSignal.subscribeNext({ [weak self] x in
+            let playing = (x as NSNumber).boolValue
+            self!.backgroundColor = playing ? UIColor.blueColor() : UIColor.whiteColor();
+        });
+    }
+}
+
 class SongViewController: UIViewController {
     let song : Song
     let player : AVPlayer
+    let frame : CGRect
+    dynamic var playing : Bool
 
     var index : Int = 0
     var songFrame : CGRect = CGRectMake(0, 0, 0, 0)
@@ -19,23 +40,28 @@ class SongViewController: UIViewController {
         assertionFailure("no")
     }
 
-    required init(song: Song) {
+    required init(frame: CGRect, song: Song) {
         self.song = song
         self.player = AVPlayer.playerWithURL(self.song.url) as AVPlayer
+        self.playing = false
+        self.frame = frame
 
         super.init(nibName: nil, bundle: nil)
 
-        self.rac_valuesForKeyPath("player.currentItem.asset.commonMetadata", observer: self).subscribeNext { array in
+        self.rac_valuesForKeyPath("player.currentItem.asset.commonMetadata", observer: self).subscribeNext { [weak self] array in
             for item in (array as [AVMetadataItem]) {
                 if (item.commonKey != "artwork" && item.commonKey != "identifier") {
-                    println(self.hash, item.stringValue)
+                    println(self!.hash, item.stringValue)
                 }
             }
         }
     }
 
     override func loadView() {
-        self.view = UIView()
+        self.view = SongView(
+            frame: self.frame,
+            song: self.song,
+            playingSignal: self.rac_valuesForKeyPath("playing", observer: self))
     }
 
     override func viewDidLoad() {
@@ -49,10 +75,10 @@ class SongViewController: UIViewController {
     func didTap() {
         if (self.player.rate == 0.0) {
             self.player.play()
-            self.view.backgroundColor = UIColor.blueColor().colorWithAlphaComponent(0.1)
+            self.playing = true
         } else {
             self.player.pause()
-            self.view.backgroundColor = UIColor.whiteColor()
+            self.playing = false
         }
     }
 }
